dbcc4690e08897a9cc6b338fac0ebbc8e4342b40
5yearsKim <hypothesis22@naver.com>
2020-09-27 16:59:44 +0900

    adding gpu

--- trainer.py
+++ trainer.py
@@ -5,7 +5,7 @@ from utils import AverageMeter
 class Trainer:
     def __init__(self, model, optimizer, criterion, train_loader, dev_loader):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = model
+        self.model = model.to(self.device)
         self.optimizer = optimizer
         self.criterion = criterion
         self.train_loader = train_loader
@@ -14,6 +14,7 @@ class Trainer:
     def train(self, epochs, print_freq=10, val_freq=1):
         loss_meter = AverageMeter()
         for epoch in range(epochs):
+            self.model.train()
             loss_meter.reset()
             for i, (x, label) in enumerate(self.train_loader):
                 self.train_step(x, label, loss_meter)
@@ -25,27 +26,40 @@ class Trainer:
                     self.validate()
 
     def train_step(self, x, label, loss_meter):
+        x, label = x.to(self.device), label.to(self.device)
         self.optimizer.zero_grad()
         logit = self.model(x)
         loss = self.criterion(logit, label)
-        loss_meter.update(loss.item())
+        loss_meter.update(loss.to('cpu').item())
         loss.backward()
         self.optimizer.step()
 
     def validate(self):
+        self.model.eval()
         loss_meter = AverageMeter()
         acc_meter = AverageMeter()
         for x, label in self.dev_loader:
+            x, label = x.to(self.device), label.to(self.device)
             logit = self.model(x)
             # loss calculation
             loss = self.criterion(logit, label)
-            loss_meter.update(loss.item())
+            loss_meter.update(loss.to('cpu').item())
             # Accuracy calculation
-            _, predicted = torch.max(logit, 1)
+            _, predicted = torch.max(logit.to('cpu'), 1)
             hit_rate = float(predicted.eq(label).sum()) / float(len(label))
             acc_meter.update(hit_rate, n=len(label))
         print(f"[Validation]: loss : {loss_meter.avg}, acc : {acc_meter.avg}")
             
+    def save(self, save_path):
+        torch.save({
+            'model_state_dict': model.state_dict(),
+            'optimizer': self.optimizer.state_dict()
+            }, save_path)
+    
+    def load(self, load_path):
+        save_dict = torch.load(load_path)
+        self.model.load_state_dict(save_dict['model_state_dict'])
+
         
 
 
